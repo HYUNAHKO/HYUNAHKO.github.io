a096114506db2c39f7a9e6c3d051d2b795dfee48
HYUNAHKO <126374997+HYUNAHKO@users.noreply.github.com>
2025-04-01 22:53:35 +0900

    Update shVert.glsl

--- hw03/shVert.glsl
+++ hw03/shVert.glsl
@@ -1,7 +1,8 @@
 #version 300 es
-layout(location = 0) in vec2 a_position;
+
+in vec2 a_position;
 
 void main(){
-    gl_PointSize = 10.0;
     gl_Position = vec4(a_position, 0.0, 1.0);
+    gl_PointSize = 10.0;
 }
